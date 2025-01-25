e7cf88076fb738967028e3f55b7d2b8169ad383a
Angela <angela.binh.hoang@gmail.com>
2025-01-25 01:24:08 -0800

    Assigment 3 part 1

--- examples/systemcalls/systemcalls.c
+++ examples/systemcalls/systemcalls.c
@@ -1,5 +1,9 @@
 #include "systemcalls.h"
-
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
 /**
  * @param cmd the command to execute with system()
  * @return true if the command in @param cmd was executed
@@ -14,10 +18,10 @@ bool do_system(const char *cmd)
  * TODO  add your code here
  *  Call the system() function with the command set in the cmd
  *   and return a boolean true if the system() call completed with success
- *   or false() if it returned a failure
+ *   or false() if it returned a failure 
 */
-
-    return true;
+    int status = system(cmd);
+    return status == 0; 
 }
 
 /**
@@ -33,36 +37,50 @@ bool do_system(const char *cmd)
 *   fork, waitpid, or execv() command, or if a non-zero return value was returned
 *   by the command issued in @param arguments with the specified arguments.
 */
-
 bool do_exec(int count, ...)
-{
-    va_list args;
-    va_start(args, count);
-    char * command[count+1];
-    int i;
-    for(i=0; i<count; i++)
-    {
-        command[i] = va_arg(args, char *);
+{ 
+    int status;
+    pid_t pid;
+    pid = fork();
+    if (pid == -1) 
+      return false;
+    // Child process
+    else if (pid == 0) {
+      va_list args;
+      va_start(args, count);
+      char* command[count+2];
+      command[0] = va_arg(args, char*);
+      int i = strlen(command[0]) - 1;
+      while (i >= 0 && command[0][i] != '/') i -= 1; 
+      i = i + 1;
+      int j = 0;
+      char buff[128];
+      while (i < strlen(command[0])) {
+        buff[j] = command[0][i];
+        i = i + 1;
+        j = j + 1;
+      }
+      buff[j] = 0;
+      command[1] = buff;
+      for (int i = 1; i < count; ++i) {
+        command[i+1] = va_arg(args, char*);
+      }
+      command[count+1] = NULL;
+      va_end(args);
+      execv(command[0], &command[1]);
+      exit(-1); // If child process is in error, return here.
+    } 
+    // Parent proc
+    int ret = waitpid(pid, &status, 0);
+    if (ret == -1) {
+      return false;
+    } else if (WIFEXITED(status)) {
+      return WEXITSTATUS(status) == 0;
     }
-    command[count] = NULL;
-    // this line is to avoid a compile warning before your implementation is complete
-    // and may be removed
-    command[count] = command[count];
-
-/*
- * TODO:
- *   Execute a system command by calling fork, execv(),
- *   and wait instead of system (see LSP page 161).
- *   Use the command[0] as the full path to the command to execute
- *   (first argument to execv), and use the remaining arguments
- *   as second argument to the execv() command.
- *
-*/
-
-    va_end(args);
-
-    return true;
+    return false;
 }
+
+
 
 /**
 * @param outputfile - The full path to the file to write with command output.
@@ -71,29 +89,53 @@ bool do_exec(int count, ...)
 */
 bool do_exec_redirect(const char *outputfile, int count, ...)
 {
-    va_list args;
-    va_start(args, count);
-    char * command[count+1];
-    int i;
-    for(i=0; i<count; i++)
-    {
-        command[i] = va_arg(args, char *);
+    int fd = open(outputfile, O_WRONLY|O_TRUNC|O_CREAT, 0644);
+    if (fd < 0) {
+      return false;
     }
-    command[count] = NULL;
-    // this line is to avoid a compile warning before your implementation is complete
-    // and may be removed
-    command[count] = command[count];
-
-
-/*
- * TODO
- *   Call execv, but first using https://stackoverflow.com/a/13784315/1446624 as a refernce,
- *   redirect standard out to a file specified by outputfile.
- *   The rest of the behaviour is same as do_exec()
- *
-*/
-
-    va_end(args);
-
-    return true;
+    int status;
+    pid_t pid;
+    pid = fork();
+    if (pid == -1) 
+      return false;
+    // Child process
+    else if (pid == 0) {
+      if (dup2(fd, 1) < 0) {
+          perror("Error duplicating the file descriptor.");
+          return false;
+      }
+      close(fd);
+      va_list args;
+      va_start(args, count);
+      char* command[count+2];
+      command[0] = va_arg(args, char*);
+      int i = strlen(command[0]) - 1;
+      while (i >= 0 && command[0][i] != '/') i -= 1; 
+      i = i + 1;
+      int j = 0;
+      char buff[128];
+      while (i < strlen(command[0])) {
+        buff[j] = command[0][i];
+        i = i + 1;
+        j = j + 1;
+      }
+      buff[j] = 0;
+      command[1] = buff;
+      for (int i = 1; i < count; ++i) {
+        command[i+1] = va_arg(args, char*);
+      }
+      command[count+1] = NULL;
+      va_end(args);
+      execv(command[0], &command[1]);
+      exit(-1); // If child process is in error, return here.
+    } 
+    // Parent proc
+    close(fd);
+    int ret = waitpid(pid, &status, 0);
+    if (ret == -1) {
+      return false;
+    } else if (WIFEXITED(status)) {
+      return WEXITSTATUS(status) == 0;
+    }
+    return false;
 }
